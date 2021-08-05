9e8e3a8c2a3adbd103ea2c0c2f9c1940520eba51
minlang <minlang@163.com>
2021-08-05 11:20:28 +0800

    管理员列表基础模板

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -20,7 +20,7 @@
 		</el-header>
 
 		<el-container>
-			<el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+			<el-aside width="200px" style="background-color: rgb(84, 92, 100)">
 				<!-- 自动打开列表1和3，例如：:default-openeds="['1','3']" -->
 				<el-menu :default-openeds="[]" background-color="#545c64" text-color="#fff">
 					<el-submenu index="1">
